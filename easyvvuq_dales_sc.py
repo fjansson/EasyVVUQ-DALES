42d78d6101d9d7569d3184021b1c7963f2888b0e
Fredrik Jansson <jansson@cwi.nl>
2019-12-10 16:07:46 +0100

    add system extent and resolution parameters. no vector params for now.

--- easyvvuq_dales_sc.py
+++ easyvvuq_dales_sc.py
@@ -21,7 +21,9 @@ dales_exe = "~/code/official-dales/build/src/dales4"
 cwd = os.getcwd()
 input_filename = 'namoptions.001'
 cmd = f"{dales_exe} {input_filename}"
-out_file = "results.json"
+#out_file = "results.json"
+out_file = "results.csv"
+
 postproc="postproc.py"
 #work_dir="/export/scratch3/jansson/uq-work/"
 work_dir="/tmp"
@@ -59,42 +61,36 @@ params = {
         "min": 0.1e6,
         "max": 1000e6,
         "default": 70e6,
-#        "unit" : "m^-3"
     },
     "cf": {  # cf subgrid filter constant
         "type": "float",
         "min": 1.0,     # min, max are just guesses 
         "max": 4.0,  
         "default": 2.5,
-#        "unit" : ""
     },
     "cn": {  # Subfilterscale parameter
         "type": "float",
         "min": 0.5,     # min, max are just guesses 
         "max": 1.0,  
         "default": 0.76,
-#        "unit" : ""
     },
     "Rigc": {  # Critical Richardson number
         "type": "float",
         "min": 0.1,     # min, max are just guesses 
         "max": 1.0,  
         "default": 0.25,
-#        "unit" : ""
     },
     "Prandtl": {  # Prandtl number, subgrid.
         "type": "float",
         "min": 0.1,     # min, max are just guesses 
         "max": 1.0,  
         "default": 1.0/3,
-#        "unit" : ""
     },
     "z0": {            # surface roughness  
         "type": "float",
         "min": 1e-4,     # min, max are just guesses 
         "max": 1.0,  
         "default": 1.6e-4,
-#        "unit" : "m"
     },
 
     "l_sb": { # flag for microphysics scheme: false - KK00 Khairoutdinov and Kogan, 2000              
@@ -102,7 +98,22 @@ params = {
         "min" : 0,  
         "max" : 1,   # converted to Fortran .true., .false. in prep.sh 
         "default" : 1
-    }
+    },
+
+    "Nh" : {
+        "type": "integer",   # number of grid points in the horizontal directions - itot, jtot
+        "min" : 3,  
+        "max" : 1024,   
+        "default" : 10
+    },
+
+    "extent": {          # Horizontal domain size in x, y  - xsize, ysize. unit: m
+        "type": "float",
+        "min": 1,     
+        "max": 1000000,  
+        "default": 1000,
+    },
+    
 }
 
 # can't have extra fields in params dict.
@@ -112,39 +123,43 @@ vary = {
 #    "cf"      : cp.Uniform(2.4, 2.6),
 #    "cn"      : cp.Uniform(0.5, 0.9),
 #    "Rigc"    : cp.Uniform(0.1, 0.4),
-    "Prandtl" : cp.Uniform(0.2, 0.4),
+#    "Prandtl" : cp.Uniform(0.2, 0.4),
 #    "z0"      : cp.Uniform(1e-4, 2e-4),
-#    "l_sb"    :  cp.DiscreteUniform(0, 1),  # discrete does not work with SC, PCE
+#    "l_sb"    :  cp.DiscreteUniform(0, 1),
+    "Nh"      : cp.DiscreteUniform(10, 20),
+    "extent"  : cp.Uniform(1000, 2000),    
 }
 
 
 
-output_columns = ['cfrac', 'lwp', 'rwp', 'zb', 'zi', 'prec', 'wq', 'wtheta', 'we']
+output_columns = ['cfrac', 'lwp', 'rwp', 'zb', 'zi', 'prec', 'wq', 'wtheta', 'we'] #, 'qt']
 unit={
      'cfrac' :'',
      'lwp'   :'g/m$^2$',
      'rwp'   :'g/m$^2$',
      'zb'    :'m',
      'zi'    :'m',
-     'prec'  : 'W/m$^2$',
+     'prec'  :'W/m$^2$',
      'wq'    :'kg/kg m/s',
      'wtheta':'K m/s',
      'we'    :'m/s',
 
-    'z0' : 'm',
-    'Nc_0' : 'm$^{-3}$',
+     'z0'    :'m',
+     'Nc_0'  :'m$^{-3}$',
+     'extent':'km'
 }
 
 scale={ 
-    'lwp'   : 1000,  # convert kg/m^2 to g/m^2
-    'rwp'   : 1000,  # convert kg/m^2 to g/m^2
+    'lwp'    : 1000,  # convert kg/m^2 to g/m^2
+    'rwp'    : 1000,  # convert kg/m^2 to g/m^2
+    'extent' : .001,  # convert m to km
 }
 
 
 # 4. Specify Sampler
 if args.sampler=='sc':
     my_sampler = uq.sampling.SCSampler(vary=vary, polynomial_order=args.order,
-                                       quadrature_rule="G")
+                                       quadrature_rule="C")
 elif args.sampler=='pce':
     my_sampler = uq.sampling.PCESampler(vary=vary, polynomial_order=args.order,
                                         quadrature_rule="G")
@@ -158,7 +173,11 @@ else:
     
 if args.prepare:
     # 1. Create campaign
-    my_campaign = uq.Campaign(name='dales', work_dir=work_dir)
+    my_campaign = uq.Campaign(name='dales', work_dir=work_dir, db_type='json')
+    # json database can contain vector-valued QoI's.
+    # the default sql database cannot, at the moment.
+
+    
     # all run directories created under workdir
 
     # 3. Wrap Application
@@ -166,14 +185,14 @@ if args.prepare:
     #    - Also requires a collation element - this will be responsible for aggregating the results
     encoder = uq.encoders.GenericEncoder(template_fname=template,
                                      target_filename=input_filename)
-    #decoder = uq.decoders.SimpleCSV(
-    #    target_filename=out_file,
-    #    output_columns=output_columns,
-    #    header=0)
-
-    decoder = JSONDecoder(
+    decoder = uq.decoders.SimpleCSV(
         target_filename=out_file,
-        output_columns=output_columns)
+        output_columns=output_columns,
+        header=0)
+
+#    decoder = JSONDecoder(
+#        target_filename=out_file,
+#        output_columns=output_columns)
 
     collater = uq.collate.AggregateSamples(average=False)
 
@@ -185,6 +204,8 @@ if args.prepare:
     )
 
     my_campaign.set_sampler(my_sampler)
+
+    my_campaign.verify_all_runs = False # to prevent errors on integer quantities
     
     # 5. Get run parameters
     if args.sampler=='random':
@@ -228,24 +249,26 @@ if args.run:
 if args.analyze:
     my_campaign = uq.Campaign(state_file=state_file_name, work_dir=work_dir)
 
-
     my_campaign.apply_for_each_run_dir(uq.actions.ExecuteLocal(postproc, interpret='python3'))
 
     # 8. Collate output
     my_campaign.collate()
 
+    data = my_campaign.get_collation_result()
+    print(data)
+        
     # 9. Run Analysis
     if args.sampler == 'random':
         analysis = uq.analysis.BasicStats(qoi_cols=output_columns)
         my_campaign.apply_analysis(analysis)
         print("stats:\n", my_campaign.get_last_analysis())
         sys.exit()
-        
+    
     if args.sampler == 'sc':
         analysis = uq.analysis.SCAnalysis(sampler=my_sampler, qoi_cols=output_columns)
     elif args.sampler == 'pce':
         analysis = uq.analysis.PCEAnalysis(sampler=my_sampler, qoi_cols=output_columns)    
-        
+    
     my_campaign.apply_analysis(analysis)
     results = my_campaign.get_last_analysis()
 
@@ -259,43 +282,46 @@ if args.analyze:
 
 
 
-    print(f"sampler: {args.sampler}, order: {args.order}")
-    print('         --- Varied input parameters ---')
-    print("  param    default      unit     distribution")
-    for k in vary.keys():
-        print("%8s %9.3g %9s  %s"%(k, params[k]['default'], unit.get(k, ''), str(vary[k])))
-    print()
+    # print(f"sampler: {args.sampler}, order: {args.order}")
+    # print('         --- Varied input parameters ---')
+    # print("  param    default      unit     distribution")
+    # for k in vary.keys():
+    #     print("%8s %9.3g %9s  %s"%(k, params[k]['default'], unit.get(k, ''), str(vary[k])))
+    # print()
 
-    print('         --- Output ---')
-    var = list(vary.keys())
+    # print('         --- Output ---')
+    # var = list(vary.keys())
 
-    print("                                            Sobol indices")
-    print("       QOI      mean       std      unit  ", end='')
-    for v in var:
-        print('%9s'%v, end=' ')
-    print()
+    # print("                                            Sobol indices")
+    # print("       QOI      mean       std      unit  ", end='')
+    # for v in var:
+    #     print('%9s'%v, end=' ')
+    # print()
 
-    for qoi in output_columns:
-        print("%10s"%qoi, end=' ')
-        print("% 9.3g % 9.3g"%(results['statistical_moments'][qoi]['mean'] * scale.get(qoi,1), results['statistical_moments'][qoi]['std'] * scale.get(qoi,1)), end=' ')
-        print("%9s"%unit[qoi], end='  ')
-        for v in var:
-            print('%9.3g'%results['sobols_first'][qoi][v], end=' ')
-        print()
+    # for qoi in output_columns:
+    #     print("%10s"%qoi, end=' ')
+    #     print("% 9.3g % 9.3g"%(results['statistical_moments'][qoi]['mean'] * scale.get(qoi,1), results['statistical_moments'][qoi]['std'] * scale.get(qoi,1)), end=' ')
+    #     print("%9s"%unit[qoi], end='  ')
+    #     for v in var:
+    #         print('%9.3g'%results['sobols_first'][qoi][v], end=' ')
+    #     print()
     
     
 
     # print(analysis.get_sample_array('cfrac'))  # just the sample values, no coordinates.
     # print(my_campaign.get_collation_result()) # a Pandas dataframe
-    data = my_campaign.get_collation_result()
 
+
+    print(data)
+    #print(data['qt'])
+    scalar_outputs = output_columns[:-1]
     #plt.plot(data['Nc_0'], data['prec'], 'o')
     params = vary.keys()
-    fig, ax = plt.subplots(nrows=len(output_columns), ncols=len(params),
+    fig, ax = plt.subplots(nrows=len(scalar_outputs), ncols=len(params),
                         sharex='col', sharey='row', squeeze=False)
 
     for i,param in enumerate(params):
-        for j,qoi in enumerate(output_columns):
+        for j,qoi in enumerate(scalar_outputs):
             x = data[param] * scale.get(param,1)
             y = data[qoi]   * scale.get(qoi,1)
             ax[j][i].plot(x, y, 'o')
